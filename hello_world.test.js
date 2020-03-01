9f34e749771785f790342961a57365942b366e1a
linnefromice <linnefromice@gmail.com>
2020-03-01 21:52:13 +0900

    add test of Hello World

--- hello_world.test.js
+++ hello_world.test.js
@@ -1,7 +1,19 @@
 const HelloWorld = require('./hello_world');
 
 test('Test_HelloWorld_01', () => {
+  const result = HelloWorld('hoge');
+  const expected = "Hello World, hoge!";
+  expect(result).toBe(expected);
+});
+
+test('Test_HelloWorld_02', () => {
+  const result = HelloWorld('12345');
+  const expected = "Hello World, 12345!";
+  expect(result).toBe(expected);
+})
+
+test('Test_HelloWorld_03', () => {
   const result = HelloWorld('');
   const expected = "Hello World!";
   expect(result).toBe(expected);
-});
+})
