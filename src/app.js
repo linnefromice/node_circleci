f5e5b6d0cf8d70ddd4a61035a303e112942f6494
linnefromice <linnefromice@gmail.com>
2020-03-03 11:27:57 +0900

    add type to all endpoints

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  res.type('html')
   res.status(200).send('Hello World!')
 })
 
@@ -13,10 +14,12 @@ const subjects = ["Japanese Language", "Mathematics", "Geography and History", "
 // https://www.mext.go.jp/a_menu/shotou/new-cs/youryou/eiyaku/__icsFiles/afieldfile/2011/04/11/1298353_0.pdf
 
 app.get('/students', (req, res) => {
+  res.type('json')
   res.status(200).json(students)
 })
 
 app.get('/subjects', (req, res) => {
+  res.type('json')
   res.status(200).json(subjects)
 })
 
