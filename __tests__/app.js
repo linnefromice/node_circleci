477925a50e4c75011635235684e20177c3690eb1
linnefromice <linnefromice@gmail.com>
2020-03-04 02:09:29 +0900

    modify test - add test content

--- __tests__/app.js
+++ __tests__/app.js
@@ -10,6 +10,7 @@ test('Test_App_01', (done) => {
 })
 
 test('Test_App_api_students_01', (done) => {
+
   request(app).get('/api/students').then((response) => {
     expect(response.status).toBe(200)
     done();
@@ -19,6 +20,7 @@ test('Test_App_api_students_01', (done) => {
 test('Test_App_api_students_id_01', (done) => {
   request(app).get('/api/students/1').then((response) => {
     expect(response.status).toBe(200)
+    expect(response.body.id).toBe(1)
     done();
   })
 })
@@ -40,6 +42,7 @@ test('Test_App_api_subjects_01', (done) => {
 test('Test_App_api_subjects_id_01', (done) => {
   request(app).get('/api/subjects/1').then((response) => {
     expect(response.status).toBe(200)
+    expect(response.body.id).toBe(1)
     done();
   })
 })
