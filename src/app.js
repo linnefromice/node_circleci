6a2ba875ad87cf70fd28308c2ce08a8de87bf015
linnefromice <linnefromice@gmail.com>
2020-03-03 11:24:32 +0900

    add reference of subjects

--- src/app.js
+++ src/app.js
@@ -9,9 +9,15 @@ app.get('/', (req, res) => {
 })
 
 const students = ["Mike", "Elie", "Matt", "Joel", "Michael"]
+const subjects = ["Japanese Language", "Mathematics", "Geography and History", "Science", "Foreign Languages"]
+// https://www.mext.go.jp/a_menu/shotou/new-cs/youryou/eiyaku/__icsFiles/afieldfile/2011/04/11/1298353_0.pdf
 
 app.get('/students', (req, res) => {
   res.status(200).json(students)
+})
+
+app.get('/subjects', (req, res) => {
+  res.status(200).json(subjects)
 })
 
 module.exports = app
