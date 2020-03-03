cf1f7e94923c6651060d16f98dab0724dc5e473e
linnefromice <linnefromice@gmail.com>
2020-03-03 11:00:21 +0900

    add httpStatus to endpoint

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,6 +3,7 @@ const app = require('../src/app')
 
 test('Test_App_01', (done) => {
   request(app).get('/').then((response) => {
+    expect(response.status).toBe(200)
     expect(response.text).toBe('Hello World!')
     done();
   })
