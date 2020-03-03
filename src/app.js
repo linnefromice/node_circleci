1b117ec37e6095b87ef4ee353fd4dccfb06f07df
linnefromice <linnefromice@gmail.com>
2020-03-03 11:21:08 +0900

    add api endpoint & modify package.json

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!')
+})
+
+const students = ["Mike", "Elie", "Matt", "Joel", "Michael"]
+
+app.get('/students', (req, res) => {
+  res.status(200).json(students)
 })
 
 module.exports = app
