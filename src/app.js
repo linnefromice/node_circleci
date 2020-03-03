9ea5a3d56e4ff3bf6e0af69a4345936b8de79eea
linnefromice <linnefromice@gmail.com>
2020-03-03 11:31:20 +0900

    add id for model used by api

--- src/app.js
+++ src/app.js
@@ -9,8 +9,20 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello World!')
 })
 
-const students = ["Mike", "Elie", "Matt", "Joel", "Michael"]
-const subjects = ["Japanese Language", "Mathematics", "Geography and History", "Science", "Foreign Languages"]
+const students = [
+  {id: 1, name: "Mike"},
+  {id: 2, name: "Elie"},
+  {id: 3, name: "Matt"},
+  {id: 4, name: "Joel"},
+  {id: 5, name: "Michael"}
+]
+const subjects = [
+  {id: 1, name: "Japanese Language"},
+  {id: 2, name: "Mathematics"},
+  {id: 3, name: "Geography and History"},
+  {id: 4, name: "Science"},
+  {id: 5, name: "Foreign Languages"}
+]
 // https://www.mext.go.jp/a_menu/shotou/new-cs/youryou/eiyaku/__icsFiles/afieldfile/2011/04/11/1298353_0.pdf
 
 app.get('/students', (req, res) => {
